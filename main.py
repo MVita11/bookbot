17732d7e92f717fe3fc73b9404496dcdc8f11042
MVita11 <mvita7698@gmail.com>
2025-12-03 19:36:45 +0000

    Updated main.py which takes all the files and counts them using sys.argv instead of hardcoring the paths

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-#from stats import get_num_words
+from stats import get_num_words
 from stats import get_char_count
+from stats import sorted_list_of_dict
+import sys
 
 def get_book_text(book):
     with open(book, "r", encoding="utf-8") as f:
@@ -7,12 +9,26 @@ def get_book_text(book):
     return file_content
 
 #def main():
-    book = "books/frankenstein.txt"
-    text = get_book_text(book)
-    print(text)
+    # book = "books/frankenstein.txt"
+    # text = get_book_text(book)
+    # print(text)
 
 
-#get_num_words(text=get_book_text("books/frankenstein.txt"))
-get_char_count(text=get_book_text("books/frankenstein.txt"))
-    
+get_num_words(text=get_book_text("books/frankenstein.txt"))
+
+char_count = get_char_count(text=get_book_text("books/frankenstein.txt"))
+
+sorted_list_of_dict(char_count=char_count)
+
 #main()
+
+#print(len(sys.argv))
+
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+for book in sys.argv[1:]:
+    text = get_book_text(book)
+    get_num_words(text=text)
+    char_count = get_char_count(text=text)
+    sorted_list_of_dict(char_count=char_count)
--- stats.py
+++ stats.py
@@ -7,11 +7,17 @@ def get_char_count(text):
     text = text.lower()
     char_count = {}
     for char in text:
+        if char.isalpha():
             if char in char_count:
                 char_count[char] += 1
             else:
                 char_count[char] = 1
-    print(char_count)
+    return char_count
 
-def sorted_dict():
-    pass    
+def sorted_list_of_dict(char_count):    
+    sorted_chars = sorted(char_count.items(), key=lambda item: item[1], reverse=True)
+    for char, count in sorted_chars:
+        print(f"{char}: {count}")
+  
+     
+    
